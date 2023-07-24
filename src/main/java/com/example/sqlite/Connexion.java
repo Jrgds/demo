aa9bc5eb4459ae7e0cd850e944711d359d8368f1
Jrgds <133639660+Jrgds@users.noreply.github.com>
2023-07-24 17:55:46 +0200

    Update Connexion.java

--- src/main/java/com/example/sqlite/Connexion.java
+++ src/main/java/com/example/sqlite/Connexion.java
@@ -1,31 +1,24 @@
-public class App  
- { 
-     public static void main( String[] args ) 
-     { 
-         System.out.println( "Hello World!" ); 
-         Connexion.bdConnexion(); 
-     } 
- }
-  
- import java.sql.Connection; 
- import java.sql.DriverManager; 
- import java.sql.SQLException; 
- import java.sql.Statement; 
-  
- public class Connexion { 
-     public static void bdConnexion(){ 
-         System.out.println("Connexion à la base de données"); 
-         String url = "jdbc:sqlite::memory:"; 
-         try { 
-             Class.forName("org.sqlite.JDBC"); 
-             try(Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement();){ 
-                 System.out.println("Connexion réussie"); 
-                 // connexion à notre base de donnée locale 
-             }catch(SQLException e){ 
-                 System.out.println(e.getMessage()); 
-             } 
-         }catch(ClassNotFoundException e){ 
-             System.out.println(e.getMessage()); 
-         } 
-     } 
- }
+package com.example.sqlite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connexion {
+    public static void bdConnexion(){
+        System.out.println("Connexion à la base de données");
+        String url = "jdbc:sqlite::memory:";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            try(Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement();){
+                System.out.println("Connexion réussie");
+                // connexion à notre base de donnée locale
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
